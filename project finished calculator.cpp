decb2423f8fb04eec5810d57911943dc99944e66
samipatel12345678 <155371078+samipatel12345678@users.noreply.github.com>
2024-01-31 17:12:39 +0530

    Add files via upload

---
+++ project finished calculator.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   //define variables
+   string operation;
+     double number1, number2;
+      
+
+      //Get the user input
+      cout<<"Enter First Number: ";
+
+      cin>>number1;
+
+      cout<<" Operation(+,-,*,/)";
+
+      cin>>operation;
+
+      cout<<"Enter Second Number: ";
+
+       cin>>number2;
+
+       //If else statement checking entered operations
+       if(operation=="+"){
+
+      cout<<" The Addition  is ";
+
+        cout<< number1 +number2;
+
+       }
+       else if(operation=="-"){
+
+         cout<<" The Substraction  is ";
+
+        cout<<number1-number2;
+
+       }
+else if(operation=="*"){
+
+        cout<<" The Multiplication  is ";
+
+        cout<<number1*number2;
+
+       }
+       else if(operation=="/"){
+
+        cout<<" The Division  is ";
+
+        cout<<number1 /number2;
+
+       }
+       
+ return 0;
+  }
+ 
